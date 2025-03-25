a281b5a95b0999699dab52db8a428fd3b61e7d67
daiki <k1tikurisu@gmail.com>
2025-03-26 01:37:40 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { DiscordHono } from 'discord-hono';
 import { handlers } from './commands';
 
 const app = new DiscordHono()
-  .command('undameshi', (context) => handlers.omikuzi({ context }))
+  .command('omikuzi', (context) => handlers.omikuzi({ context }))
   .command('time', async (context) => await handlers.time({ context }));
 
 export default app;

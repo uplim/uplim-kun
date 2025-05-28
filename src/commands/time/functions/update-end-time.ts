2e9de2beff33fc2c47dd81ea87f3dd2563397dfa
daiki <k1tikurisu@gmail.com>
2025-05-29 01:35:07 +0900

    jstを明確か

--- src/commands/time/functions/update-end-time.ts
+++ src/commands/time/functions/update-end-time.ts
@@ -1,6 +1,6 @@
 import { get } from '../../../clients/spreadsheets/values/get';
 import { update } from '../../../clients/spreadsheets/values/update';
-import { formatJSTDatetime } from '../../../functions/format-jst-datetime';
+import { formatDatetime } from '../../../functions/format-datetime';
 import { getJstDate } from '../../../functions/get-jst-date';
 import type { Result } from '../../../types/result';
 import { getSheets } from './get-sheets';
@@ -24,7 +24,7 @@ export const updateEndTIme = async ({
   const sheetList = await getSheets({ env });
 
   let endedProject = undefined;
-  const endTime = getJstDate();
+  const endTimeJst = getJstDate();
 
   // 全てのシートをループして未終了の勤務を探す
   for (const [sheetTitle] of Object.entries(sheetList)) {
@@ -37,8 +37,8 @@ export const updateEndTIme = async ({
     // ヘッダー行をスキップして検索
     for (let i = 1; i < rows.length; i++) {
       if (rows?.[i]?.[0] === userId && rows?.[i]?.[2] && !rows?.[i]?.[3]) {
-        const startTime = new Date(rows?.[i]?.[2] ?? '');
-        const totalHours = (endTime.getTime() - startTime.getTime()) / (1000 * 60 * 60);
+        const startTimeJst = new Date(rows?.[i]?.[2] ?? '');
+        const totalHours = (endTimeJst.getTime() - startTimeJst.getTime()) / (1000 * 60 * 60);
 
         // 終了時間と勤務時間を更新
         await update({
@@ -46,14 +46,14 @@ export const updateEndTIme = async ({
           valueInputOption: 'USER_ENTERED',
           range: `${sheetTitle}!D${i + 1}:E${i + 1}`,
           requestBody: {
-            values: [[formatJSTDatetime(endTime), totalHours.toFixed(2)]],
+            values: [[formatDatetime(endTimeJst), totalHours.toFixed(2)]],
           },
         });
 
         endedProject = {
           projectName: sheetTitle,
-          startTime: formatJSTDatetime(startTime),
-          endTime: formatJSTDatetime(endTime),
+          startTime: formatDatetime(startTimeJst),
+          endTime: formatDatetime(endTimeJst),
           totalHours: totalHours.toFixed(2),
         };
 
